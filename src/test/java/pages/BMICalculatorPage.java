bd20b802d296ff861483fa9a7749d84cba0e18de
buikhanhhuyen <buikhanhhuyenptit@gmail.com>
2022-07-02 20:51:25 +0700

    allure report

--- src/test/java/pages/BMICalculatorPage.java
+++ src/test/java/pages/BMICalculatorPage.java
@@ -59,15 +59,15 @@ public class BMICalculatorPage {
     }
 
     public void inputHeight(double height){
-        waitUntilElementVisible(driver.findElement(heightInput));
-//        driver.findElement(heightInput).sendKeys(String.valueOf(format.format(height)));
+//        waitUntilElementVisible(driver.findElement(heightInput));
+        driver.findElement(heightInput).sendKeys(String.valueOf(format.format(height)));
 
 //        je.executeScript("document.getElementById('" + heightInput + "').value = '" + String.valueOf(format.format(height)) + "'");
 //        je.executeScript("document.getElementById('" + heightInput + "').setAttribute('value', '" + String.valueOf(format.format(height)) + "')");
-        JavascriptExecutor je = (JavascriptExecutor) driver;
-        String xpath = "document.evaluate(\"//input[@id='cheightmeter']\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)";
+//        JavascriptExecutor je = (JavascriptExecutor) driver;
+//        String xpath = "document.evaluate(\"//input[@id='cheightmeter']\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)";
 //        je.executeScript("document.evaluate(" + xpath + ").singleNodeValue.value=" + String.valueOf(format.format(height)) + ";");
-        je.executeScript(xpath + "singleNodeValue.value=" + String.valueOf(format.format(height)) + ";");
+//        je.executeScript(xpath + "singleNodeValue.value=" + String.valueOf(format.format(height)) + ";");
     }
 
     public void inputWeight(double weight){
