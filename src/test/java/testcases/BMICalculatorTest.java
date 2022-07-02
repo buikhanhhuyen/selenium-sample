29812b5815b28d0eea6a20ea0998ae3c37371185
buikhanhhuyen <buikhanhhuyenptit@gmail.com>
2022-07-02 22:17:08 +0700

    remove sleep

--- src/test/java/testcases/BMICalculatorTest.java
+++ src/test/java/testcases/BMICalculatorTest.java
@@ -21,25 +21,20 @@ public class BMICalculatorTest extends BrowserUtils implements BMICalculatorUtil
     @DataProvider
     Object[][] validDataTest(){
         return new Object[][]{
-                {25, "male", 180, 65}
-//                {29.34, "male", 180.9032, 70.23423},
-//                {20, "female", 150, 55},
-//                {2, "female", 0.5, 0.5},
-//                {120, "male", 2000, 160}
+                {25, "male", 180, 65},
+                {29.34, "male", 180.9032, 70.23423},
+                {20, "female", 150, 55},
+                {2, "female", 0.5, 0.5},
+                {120, "male", 2000, 160}
         };
     }
     @Test(dataProvider = "validDataTest")
     void verifyCalculateWithValidTestData(double age, String gender, double height, double weight) throws InterruptedException {
         page = new BMICalculatorPage(driver);
-        Thread.sleep(2000);
         page.selectMetricUnitTab();
-        Thread.sleep(2000);
         page.clickClearBtn();
-        Thread.sleep(1000);
         page.fillForm(age, gender, height, weight);
-        Thread.sleep(1000);
         page.clickCalculateBtn();
-        Thread.sleep(3000);
         String actualResult = page.getResult();
         String expectedResult = bmiResult(height, weight);
         Assert.assertEquals(actualResult, expectedResult);
