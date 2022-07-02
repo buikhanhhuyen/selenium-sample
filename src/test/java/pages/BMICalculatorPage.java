c4a3d4a69b7be30b41ed34b9fa22e8410617bc7f
buikhanhhuyen <buikhanhhuyenptit@gmail.com>
2022-07-02 22:02:42 +0700

    Update element locator

--- src/test/java/pages/BMICalculatorPage.java
+++ src/test/java/pages/BMICalculatorPage.java
@@ -9,7 +9,8 @@ import java.time.Duration;
 
 public class BMICalculatorPage {
     private WebDriver driver;
-    By metricUnitTab = By.xpath("//*[@id='menuon']//a");
+//    By metricUnitTab = By.xpath("//*[@id='menuon']//a");
+    By metricUnitTab = By.linkText("Metric Units")
     By ageInput = By.id("cage");
     By maleRad = By.xpath("//label[@class='cbcontainer'][1]");
     By femaleRad = By.xpath("//label[@class='cbcontainer'][2]");
@@ -38,6 +39,7 @@ public class BMICalculatorPage {
 
     public void selectMetricUnitTab(){
         driver.findElement(metricUnitTab).click();
+//        driver.findElement(By.linkText("Metric Units")).click();
     }
 
     public void clickClearBtn(){
